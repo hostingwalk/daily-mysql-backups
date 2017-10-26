51fa4bb0c4c9d40421a0e38a880ba1ca648796d5
Hostingwalk <abuse@hostingwalk.com>
2017-10-26 15:38:16 +0200

    Create backupsql.sh

---
+++ backupsql.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# MySQL/MariaDB	Back-up	script      
+# Requirments: enough free space / Directadmin
+# ---------------------------------------------------------------------
+
+USER="da_admin"
+PASSWORD="`grep passwd= /usr/local/directadmin/conf/mysql.conf | cut -d\= -f2`"
+#OUTPUT="/Users/rabino/DBs"
+
+#rm "$OUTPUTDIR/*gz" > /dev/null 2>&1
+
+databases=`mysql -u $USER -p$PASSWORD -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
+
+for db in $databases; do
+    if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" $
+        echo "Dumping database: $db"
+        mysqldump -u $USER -p$PASSWORD --databases $db | gzip > `date +%Y%m%d`.$db.sql.gz
+       # gzip $OUTPUT/`date +%Y%m%d`.$db.sql
+    fi
+done
