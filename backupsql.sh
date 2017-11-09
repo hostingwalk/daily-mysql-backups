e2c6ec18f0a0a56a2f5d210dbecffa1d522cc9f8
Hostingwalk <abuse@hostingwalk.com>
2017-11-09 20:41:04 +0100

    added remove old back-ups so only .gz files

--- backupsql.sh
+++ backupsql.sh
@@ -4,7 +4,7 @@ USER="da_admin"
 PASSWORD="`grep passwd= /usr/local/directadmin/conf/mysql.conf | cut -d\= -f2`"
 #OUTPUT="/Users/rabino/DBs"
 
-#rm "$OUTPUTDIR/*gz" > /dev/null 2>&1
+find /root/mysql-backups -name '*.gz' -type f -mtime -1 -exec rm -f {} \;
 
 databases=`mysql -u $USER -p$PASSWORD -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
 
