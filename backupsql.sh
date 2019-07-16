ed4391f8e84314e477cee336af4fe006b4312083
Hostingwalk <abuse@hostingwalk.com>
2019-07-16 15:20:46 +0200

    Update backupsql.sh

--- backupsql.sh
+++ backupsql.sh
@@ -2,16 +2,20 @@
 
 USER="da_admin"
 PASSWORD="`grep passwd= /usr/local/directadmin/conf/mysql.conf | cut -d\= -f2`"
+BACKUP_RETAIN_DAYS=1   ## Number of days to keep local backup copy
+DB_BACKUP_PATH='/home/mysql-backups'
+TODAY=`date +"%Y%m%d"`
+
 #OUTPUT="/Users/rabino/DBs"
 
-find /root/mysql-backups -name '*.gz' -type f -mtime -1 -exec rm -f {} \;
+find ${DB_BACKUP_PATH} -name '*.gz' -type f -mtime +${BACKUP_RETAIN_DAYS} -exec rm -f {} \;
 
 databases=`mysql -u $USER -p$PASSWORD -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
 
 for db in $databases; do
     if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] ; then
         echo "Dumping database: $db"
-        mysqldump -u $USER -p$PASSWORD --databases $db | gzip > `date +%Y%m%d`.$db.sql.gz
+        mysqldump -u $USER -p$PASSWORD --databases $db | gzip > ${TODAY}.$db.sql.gz
        # gzip $OUTPUT/`date +%Y%m%d`.$db.sql
     fi
 done
